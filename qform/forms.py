e588a426893183a5a898c54cba8a8ea6082af414
Evan Zhang <zhang21evan@ncssm.edu>
2020-06-26 14:05:23 -0400

    Commit

--- qform/forms.py
+++ qform/forms.py
@@ -1,12 +1,23 @@
 from django import forms
 from django.contrib.auth.models import User
 
+GENDER_CHOICES = [
+    ('male', 'Male'),
+    ('female', 'Female'),
+    ('other', 'Other')
+]
+
+MY_CHOICES = (('item_key1', 'Item title 1.1'),
+              ('item_key2', 'Item title 1.2'),
+              ('item_key3', 'Item title 1.3'),
+              ('item_key4', 'Item title 1.4'),
+              ('item_key5', 'Item title 1.5'))
+
 class UserQuestionForm(forms.Form):
     name = forms.CharField()
     age = forms.DecimalField()
+    gender = forms.CharField(widget=forms.Select(choices=GENDER_CHOICES))
     country = forms.CharField()
-    country1 = forms.CharField()
-    country2 = forms.CharField()
-    country3 = forms.CharField()
-    country4 = forms.CharField()
-    country5 = forms.CharField()
+    interests = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=MY_CHOICES)
+
+
