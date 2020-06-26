5e4c202e8a28441feccdaf3d85fa14b302f3df27
Evan Zhang <zhang21evan@ncssm.edu>
2020-06-26 13:45:30 -0400

    Commit

--- qform/forms.py
+++ qform/forms.py
@@ -5,3 +5,8 @@ class UserQuestionForm(forms.Form):
     name = forms.CharField()
     age = forms.DecimalField()
     country = forms.CharField()
+    country1 = forms.CharField()
+    country2 = forms.CharField()
+    country3 = forms.CharField()
+    country4 = forms.CharField()
+    country5 = forms.CharField()
